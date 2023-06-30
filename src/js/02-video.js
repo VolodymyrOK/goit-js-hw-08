624cb29a69fe866413708bb642847e9089d9f1ed
Volodymyr <vladikon70@gmail.com>
2023-06-30 21:52:33 +0200

    Add task №2

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,26 @@
+import Player from '@vimeo/player';
+const throttle = require('lodash.throttle');
+// console.log(Player);
 
+const iframe = document.querySelector('iframe');
+const player = new Player(iframe);
+
+const saveTime = localStorage.getItem('videoplayer-current-time');
+
+player.setCurrentTime(saveTime).catch(function (error) {
+  switch (error.name) {
+    case 'RangeError':
+      alert('Playback error.Reload the page.');
+      break;
+
+    default:
+      alert('Playback error. Press the play button.');
+      break;
+  }
+});
+
+const onPlay = function (data) {
+  localStorage.setItem('videoplayer-current-time', data.seconds);
+};
+
+player.on('timeupdate', throttle(onPlay, 1000));
